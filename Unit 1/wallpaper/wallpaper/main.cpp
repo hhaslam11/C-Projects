96f8b3ac045872e2aba74f51d12e8a349370187a
Kaleb Haslam <kalebmarc@hotmail.com>
2016-03-21 10:45:50 -0700

    Added Wallpaper project

---
+++ Unit 1/wallpaper/wallpaper/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+	
+	double length      = 0,
+		   width       = 0,
+		   height      = 0,
+		   rollCov     = 0,
+		   perm        = 0,
+		   wallArea    = 0,
+		   rollsNeeded = 0;
+
+	//Input
+	cout << "Room length: ";
+	cin >> length;
+	cout << "Room width: ";
+	cin >> width;
+	cout << "Room height: ";
+	cin >> height;
+	cout << "Single roll coverage: ";
+	cin >> rollCov;
+
+	//Calculations
+	perm = (length * 2) + (width * 2);
+	wallArea = perm * height;	
+	rollsNeeded = wallArea / rollCov;
+
+	//Output
+	cout << "Rolls needed: " << rollsNeeded;
+
+	system("pause");
+	return 0;
+}
