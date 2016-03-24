c1cf5f2b2e425287570047db0f9cd414b17fc4d7
Kaleb Haslam <kalebmarc@hotmail.com>
2016-03-24 14:20:00 -0700

    Started FatFarm project

---
+++ Unit 2/Fat Farm/Fat Farm/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+int(){
+	
+	double total, fat, caloriesDueToFat, percent;
+	//input
+	cout << "Total Calories: ";
+	cin >> total;
+	cout << "Grams of fat: ";
+	cin >> fat;
+
+	//calculations
+	caloriesDueToFat = total - (total - (fat * 9))
+	percent          = 
+
+	//output
+
+	return 0;
+}
+//Kaleb Haslam
+//Fat Farm
