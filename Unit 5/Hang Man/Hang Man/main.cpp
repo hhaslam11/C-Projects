f1460dd19b8931b9f2faa696b374407e0e7c8f21
Kaleb Haslam <kalebmarc@hotmail.com>
2016-06-01 10:57:13 -0700

    Finished Hangman

--- Unit 5/Hang Man/Hang Man/main.cpp
+++ Unit 5/Hang Man/Hang Man/main.cpp
@@ -7,26 +7,58 @@ using std::endl;
 using std::string;
 
 int main(){
-	
+		
 	string word;
+	string wordOriginal;
+	bool correct = false; //Is player guess correct
 	char currGuess;
-	
-	cout << "Enter word" << endl;
-	system("cls");
-	const int wordLength = static_cast<int>(word.length());
-	char guesses[wordLength];
+	char guesses[50];
+	int wrongGuesses = 0;
 
-	cout << "Guess a letter" << endl;
-	cin >> currGuess;
+	//Get word
+	cout << "Enter word" << endl;
+	cin >> word;
+	system("cls");
+	int wordLength = static_cast<int>(word.length());
+	wordOriginal = word;
 	
-	if(word.find(currGuess, 0) != -1){
-		//guesses
-	}
+	//Build array
+	for (int i = 0; i < wordLength; i++)
+		guesses[i] = '_';
 	
-	for (int i = 0; i < static_cast<int>(word.length()); i++)
-		cout << "_ ";
-	cout << endl;
+	while(wrongGuesses < 10){
+
+		//Draw
+		for (int i = 0; i < wordLength; i++)
+			cout << guesses[i] << " ";
+		cout << endl;
+		cout << "Wrong guesses: " << wrongGuesses << "/10" << endl;
+
+		//Guess letter
+		cout << "Guess a letter" << endl;
+		cin >> currGuess;
+		
+		//Test input
+		for (int i = 0; i < wordLength; i++)
+			if(word.find(currGuess, 0) != -1){
+				guesses[word.find(currGuess, 0)] = currGuess;
+				word.replace(word.find(currGuess, 0), 1, "#");
+				correct = true;
+			}
+
+		if(!correct)
+			wrongGuesses++;
+		else
+			correct = false;//reset
+		
+		if(string(guesses).compare(0, wordLength, wordOriginal) == 0){
+			cout << "Correct!" << endl;
+			system("pause");
+			return 0;
+		}		
+	}//while
 	
+	cout << "Failed." << endl;
 	system("pause");
 	return 0;
 }
