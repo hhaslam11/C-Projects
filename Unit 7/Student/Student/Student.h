7f66c4a9f745f0a88e75657cefb006cec5c75445
Kaleb Haslam <kalebmarc@hotmail.com>
2016-06-17 13:59:23 -0700

    Started studentOOP project

---
+++ Unit 7/Student/Student/Student.h
@@ -0,0 +1,63 @@
+#include <math.h>
+
+class Student{
+public:
+	Student();
+	Student(double, double, double);
+	void setNameFirst(String );
+	String getNameFirst();
+	void setNameLast(String );
+	String getNameLast();
+	void setAge(int);
+	int getAge();
+	void setGender(Gender);
+	Gender getGender();
+	void setStudentNumber(int);
+	int getStudentNumber();
+	void setAncestry(bool);
+	bool getAncestry();
+
+private:
+	enum Gender(Male, Female, Other);
+	String nameFirst;
+	String nameLast;
+	int age;
+	Gender gender;
+	int studentNumber;
+	bool ancestry;
+
+};
+Student::Student(){
+	this->width  = 0.00;
+	this->height = 0.00;
+}
+Student::Student(double width, double height, double price){
+	this->width  = width;
+	this->height = height;
+}
+void Student::setNameFirst(String name){}
+String Student::getNameFirst(){
+	return this->nameFirst ;
+}
+void Student::setNameLast(String name){}
+String Student::getNameLast(){
+	return this->nameLast ;
+}
+void Student::setAge(int age){}
+int Student::getAge(){
+	return this->age ;
+}
+void Student::setGender(Gender gender){}
+Gender Student::getGender(){
+	return this->gender ;
+}
+void Student::setStudentNumber(int number){}
+int Student::getStudentNumber(){
+	return this->studentNumber ;
+}
+void Student::setAncestry(bool ancest){}
+bool Student::getAncestry(){
+	return this->ancestry ;
+}
+
+//Student.h
---
+++ Unit 7/Student/Student/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <String>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::String;
+
+int main(){
+	
+	return 0;
+}
+
+/*
+ * Kaleb Haslam
+ * Student grade thing using OOP
+ */
